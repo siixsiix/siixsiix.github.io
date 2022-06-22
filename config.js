7d931dfc9dc58e2087f910f350ceb3802045d269
siixsiix <108004966+siixsiix@users.noreply.github.com>
2022-06-22 20:15:02 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,12 +1,3 @@
-# Tặng Crush
-## _Một điều nho nhỏ tỏ tình với crush_
-
-Liên lạc: 
-[![Facebook](https://i.imgur.com/GRqy96ts.jpg)](https://www.facebook.com/nam.nodemy)
-[![Tiktok](https://i.imgur.com/Nbfl1E7t.jpg)](https://www.tiktok.com/@manindev)
-
-Mở file config.js sửa nội dung theo mẫu
-```
 const CONFIG = {
     introTitle: 'Babe à!',
     introDesc: `Trái đất vốn lạ thường
@@ -26,5 +17,3 @@ const CONFIG = {
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
-```
-
